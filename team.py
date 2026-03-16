64ed1ff5c603f2a6be72c3c2d775b65da7e47ca6
arpit-sigmoid <arpit.aggarwal@sigmoidanalytics.com>
2026-03-16 18:54:21 +0530

    this is changes done by main

--- team.py
+++ team.py
@@ -1,9 +1,9 @@
 #Arpit TSV-886
 def greet():
-    print("HI, I am Arpit Aggarwal  and this is what i am changing TSV-886")
+    print("HI, I am Arpit Aggarwal  and this is what i am changing TSV-886 wowwo")
 
 
 def farewell():
-    print("Bye,Have a good day and see you later ")
+    print("Bye,Have a good day and see you later  wow you are gere")
 greet()
 farewell()
